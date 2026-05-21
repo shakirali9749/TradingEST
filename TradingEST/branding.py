93597199980e574cbaa0e2e13dbc8310be18b473
uzairr <uzairr@yahoo.com>
2026-05-21 17:41:33 +0500

    Englis Brand name changed

--- TradingEST/branding.py
+++ TradingEST/branding.py
@@ -2,5 +2,5 @@
 
 # مزدهر = prosperous (ز has the distinguishing dot; matches English "Future")
 BRAND_NAME_AR = "المستقبل المزدهر التجارية"
-BRAND_NAME_EN = "PROSPEROUS FUTURE EST"
+BRAND_NAME_EN = "PROSPEROUS FUTURE TRADING EST"
 BRAND_TAGLINE = "Powering Industries with Precision & Reliability"
